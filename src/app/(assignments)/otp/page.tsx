8d8bf8ebd3002c8cad688ea0203c8d86fc1905d7
Nihar Hegde <niharhegde163@gmail.com>
2024-01-19 19:57:50 +0530

    add otp

--- src/app/(assignments)/otp/page.tsx
+++ src/app/(assignments)/otp/page.tsx
@@ -1,18 +1,54 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
+let currentOtpIndex: number = 0;
 const OtpPage = () => {
   const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
+  const [activeOTPIndex, setActiveOTPIndex] = useState<number>(0);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleOnChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    const newOtp: string[] = [...otp];
+    newOtp[currentOtpIndex] = value.substring(value.length - 1);
+    if (!value) {
+      setActiveOTPIndex(currentOtpIndex - 1);
+    } else {
+      setActiveOTPIndex(currentOtpIndex + 1);
+    }
+
+    setActiveOTPIndex(currentOtpIndex + 1);
+    setOtp(newOtp);
+  };
+
+  const handleOnKeyDown = (
+    { key }: React.KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    currentOtpIndex = index;
+    if (key === "Backspace") {
+      setActiveOTPIndex(currentOtpIndex - 1);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOTPIndex]);
+
   return (
     <div className="h-screen flex justify-center items-center space-x-2">
       {otp.map((_, index) => {
         return (
           <React.Fragment key={index}>
             <input
+              ref={index === activeOTPIndex ? inputRef : null}
               type="number"
               className="w-12 h-12 border-2 rounded bg-transparent outline-none text-center font-semibold text-xl border-gray-400 
               focus:border-gray-700 focus:text-gray-700 text-gray-400 transition spin-button-none
               "
+              onChange={handleOnChange}
+              onKeyDown={(e) => handleOnKeyDown(e, index)}
+              value={otp[index]}
             />
             {index === otp.length - 1 ? null : (
               <span className="w-2 py-0.5 bg-gray-400" />
